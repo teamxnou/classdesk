59dc223ccdbd1dc1e1ecfa68e9d8f4f292d27dbb
xnousnow <xnou.snow@gmail.com>
2023-06-25 20:31:28 +0900

    :lipstick: ClassDesk 로고 추가

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,11 @@
-<h1>Hello, world!</h1>
+<script lang="ts">
+  import Logo from '../components/Logo.svelte'
+</script>
+
+<div class="flex h-screen w-screen justify-center bg-neutral-100">
+  <main class="container mx-3 mt-10 flex max-w-4xl flex-col sm:mx-8 lg:mx-10 xl:mx-10">
+    <header>
+      <Logo />
+    </header>
+  </main>
+</div>
