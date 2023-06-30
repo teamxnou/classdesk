ed56587ab9723aa3411633980d0268c7ae310402
xnousnow <xnou.snow@gmail.com>
2023-06-30 16:30:45 +0900

    :fire: 사용되지 않는 css 삭제

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -127,11 +127,6 @@
 </div>
 
 <style lang="postcss">
-  #favorite ul li a {
-    @apply flex items-center gap-2 rounded-xl px-3 py-2
-      transition duration-150 hover:-translate-y-1 lg:h-20 lg:w-24 lg:flex-col lg:justify-between
-      lg:gap-0 lg:pb-2 lg:pt-3;
-  }
   ul.big-items {
     @apply grid gap-3;
   }
