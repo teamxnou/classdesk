dd49d69b40ae527b40c7898644d08b9023a7204f
xnousnow <xnou.snow@gmail.com>
2023-09-07 16:09:50 +0900

    :sparkles: 날짜 버튼 구현

--- src/routes/tools/whiteboardtext/+page.svelte
+++ src/routes/tools/whiteboardtext/+page.svelte
@@ -5,20 +5,43 @@
   import Type from '~icons/lucide/Type'
   import Trash from '~icons/lucide/Trash'
   import Clipboard from '~icons/lucide/Clipboard'
+
+  const date = new Date().toLocaleDateString('ko-KR', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    weekday: 'long'
+  })
+  let showDate = false
 </script>
 
 <div class="flex h-screen w-screen flex-col gap-5 bg-neutral-100 p-12">
   <div class="flex w-full items-center justify-between gap-10">
-    <input
-      type="text"
-      class="grow bg-transparent text-6xl font-bold placeholder-neutral-400 focus:outline-none"
-      placeholder="제목을 입력하세요..."
-    />
+    <div
+      class="flex grow cursor-default"
+      on:click={() => (showDate = false)}
+      on:keydown={(e) => e.key === '/' && (showDate = false)}
+      role="button"
+      tabindex="0"
+    >
+      <input type="text" class="grow text-6xl font-bold" disabled value={date} hidden={!showDate} />
+      <input
+        type="text"
+        class="grow bg-transparent text-6xl font-bold placeholder-neutral-400 focus:outline-none"
+        placeholder="제목을 입력하세요..."
+        hidden={showDate}
+      />
+    </div>
     <label>
-      <input type="checkbox" class="peer hidden" />
+      <input type="checkbox" class="peer hidden" bind:checked={showDate} />
       <div
-        class="cursor-pointer rounded-lg p-3 transition-[color] duration-150 hover:bg-neutral-200 peer-checked:text-blue-500 peer-checked:hover:bg-blue-100 peer-checked:hover:text-blue-600"
+        class="group flex cursor-pointer rounded-lg p-3 transition-[color] duration-150 hover:bg-neutral-200 peer-checked:text-blue-500 peer-checked:hover:bg-blue-100 peer-checked:hover:text-blue-600"
       >
+        <span
+          class="mr-2 hidden font-semibold opacity-0 transition duration-150 group-hover:inline group-hover:opacity-100"
+        >
+          날짜 보이기
+        </span>
         <Calander class="h-6 w-6" />
       </div>
     </label>
